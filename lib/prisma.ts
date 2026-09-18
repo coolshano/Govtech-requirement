85c64eec702602c8dd7db54ceae00e2a90940333
Shannon Smith <silveraldrin@gmail.com>
2026-09-18 15:01:04 +0530

    added

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
-export default prisma;
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
